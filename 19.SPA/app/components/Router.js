b744ec19d235669a6f07b6e46ea2123c67334d60
Sebastian <sebasti1959@hotmail.com>
2022-03-18 00:48:00 -0500

    Se agrega un buscador que se conecta con el endpoint de wordpress busquedas y devuelve por consola un array con los resultados

--- 19.SPA/app/components/Router.js
+++ 19.SPA/app/components/Router.js
@@ -5,8 +5,20 @@ import { Post } from "./Post.js";
 
 export async function Router() {
   let { hash } = location,
+    search = "",
     $main = document.getElementById("main");
+
   $main.innerHTML = null;
+
+  document.addEventListener("submit", (e) => {
+    if (!e.target.matches(".search-form")) return false;
+    if (e.target.matches(".search-form")) {
+      e.preventDefault();
+      search = document.querySelector("input").value;
+      localStorage.setItem("wpSearch", search);
+      location.hash = `#/search?search=${search}`;
+    }
+  });
 
   function loaderOff() {
     document.querySelector(".loader").style.display = "none";
@@ -18,7 +30,7 @@ export async function Router() {
       callBackSuccess: (posts) => {
         let html = "";
 
-        console.log(posts);
+        // console.log(posts);
 
         posts.forEach((post) => {
           html += PostCard(post);
@@ -28,13 +40,22 @@ export async function Router() {
         $main.innerHTML = html;
       },
     });
-  } else if (hash === "#/search") {
-    $main.innerHTML = "<h2>Sección del Buscador.</h2>";
+  } else if (hash.includes("#/search")) {
+    let query = localStorage.getItem("wpSearch");
+    if (!query) localStorage.removeItem("wpSearch");
+
+    await ajax({
+      url: `${api.SEARCH}${query}`,
+      callBackSuccess: (search) => {
+        console.log("query");
+        console.log(search);
+      },
+    });
   } else if (hash === "#/contacto") {
     $main.innerHTML = "<h2>Sección del contacto.</h2>";
     console.log(api.POST);
   } else {
-    let urls = window.location.hash.slice(2);
+    let urls = hash.slice(2);
 
     await ajax({
       url: `${api.POST}?slug=${urls}`,
